40c93a0c9ba96cb13cb9cd8c6484877eeb45f86c
Codinator29 <67217624+Codinator29@users.noreply.github.com>
2026-09-05 14:03:19 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int result = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            if(i != nums[i]){
+                return i;
+            }
+        }
+
+        return nums.length;
+    }
+}
